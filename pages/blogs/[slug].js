112162f6bd104af1acfe0215814981eb6c9d3bb1
Gitravs <kitravee.swk@gmail.com>
2020-08-01 02:32:37 +0700

    Get blog data and getServerSideProps

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,14 +1,22 @@
 import PageLayout from 'components/PageLayout';
 import { useRouter } from 'next/router';
+import { getBlogBySlug } from 'lib/api';
 
-const BlogDetail = () => {
-  const { query } = useRouter();
-
+const BlogDetail = ({ blog }) => {
+  //   const { query } = useRouter();
+  debugger;
   return (
     <PageLayout>
-      <h1>h1 {query.slug}</h1>
+      <h1>h1 {blog?.slug}</h1>
     </PageLayout>
   );
 };
+
+export async function getServerSideProps({ params }) {
+  const blog = await getBlogBySlug(params.slug);
+  return {
+    props: { blog }, // will be passed to the page component as props
+  };
+}
 
 export default BlogDetail;
