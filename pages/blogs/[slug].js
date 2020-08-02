375752df359bf2e1390f70730faa6f7146a572e0
Gitravs <kitravee.swk@gmail.com>
2020-08-02 15:05:28 +0700

    Dynamic slug

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,13 +1,29 @@
 import PageLayout from 'components/PageLayout';
+import BlogHeader from 'components/BlogHeader';
 import { useRouter } from 'next/router';
-import { getBlogBySlug } from 'lib/api';
-
+import { getBlogBySlug, getAllBlogs } from 'lib/api';
+import { Row, Col } from 'react-bootstrap';
 const BlogDetail = ({ blog }) => {
   //   const { query } = useRouter();
-  debugger;
+  // debugger;
   return (
-    <PageLayout>
-      <h1>h1 {blog?.slug}</h1>
+    <PageLayout className='blog-detail-page'>
+      <Row>
+        <Col md={{ span: 10, offset: 1 }}>
+          <BlogHeader blog={blog} />
+          <hr />
+          {/* Blog Content Here */}
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </Col>
+      </Row>
     </PageLayout>
   );
 };
@@ -27,14 +43,13 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
+  const blogs = await getAllBlogs();
+
   return {
-    paths: [
-      { params: { slug: 'my-first-blog' } },
-      { params: { slug: 'my-second-blog' } },
-      { params: { slug: 'ggg' } }, // See the "paths" section below
-    ],
+    paths: blogs?.map((blog) => ({
+      params: { slug: blog.slug },
+    })),
     fallback: false,
-    //   fallback: true or false // See the "fallback" section below
   };
 }
 
